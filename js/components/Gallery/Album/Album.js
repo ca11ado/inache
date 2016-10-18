4b446a814b2327db779a927f5d72fcaa31e4b4da
Химцов Антон <t00s@ya.ru>
2016-10-19 01:56:23 +0300

    add some images for testing gallery

--- js/components/Gallery/Album/Album.js
+++ js/components/Gallery/Album/Album.js
@@ -7,18 +7,20 @@ let SubNavigation = require('../../sub-navigation/subNavigation');
 let MainBlock = require('../../main-block/mainBlock');
 let Content = require('../../main-block/content/content');
 
+let Lightbox = require('react-images');
+
 let Album = React.createClass({
   getInitialState () {
     return {};
   },
 
-  componentDidMount: function() {
+  componentDidMount: function () {
   },
 
-  componentWillUnmount: function() {
+  componentWillUnmount: function () {
   },
 
-  componentWillReceiveProps: function(nextProps) {
+  componentWillReceiveProps: function (nextProps) {
   },
 
   render () {
@@ -28,14 +30,29 @@ let Album = React.createClass({
     return (
       <MainBlock>
         <Header>Фото</Header>
-        <SubNavigation base='gallery' list={backLink} />
+        <SubNavigation base='gallery' list={backLink}/>
         <Content>
           <div>
             <h2>Here will be album's photos</h2>
+            <Lightbox
+              images={[
+                { src: '/img/albums/1463762230000/1.jpg' },
+                { src: '/img/albums/1463762230000/2.jpg' },
+                { src: '/img/albums/1463762230000/2.jpg' }
+              ]}
+              isOpen={this.state.lightboxIsOpen}
+              onClickPrev={this.gotoPrevious}
+              onClickNext={this.gotoNext}
+              onClose={this.closeLightbox}
+            />
           </div>
         </Content>
       </MainBlock>
     );
+  },
+
+  closeLightbox () {
+
   }
 });
 
