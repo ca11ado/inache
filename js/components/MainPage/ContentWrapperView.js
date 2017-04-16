54f8f5c3236c60068dccf98ed738c264761eec53
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2017-04-16 21:53:31 +0300

    FEATURE-animation-main-page - enable main page animation

--- js/components/MainPage/ContentWrapperView.js
+++ js/components/MainPage/ContentWrapperView.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const CSSTransitionGroup = require('react-transition-group/CSSTransitionGroup');
 const css = require('./content-wrapper-view.css');
 const CenterBlock = require('./Blocks/CenterBlockView');
 const LeftBlock = require('./Blocks/LeftBlockView');
@@ -7,11 +8,18 @@ const RightBlock = require('./Blocks/RightBlockView');
 const CenterBlockView = (props) => {
 
   return (
-    <div className={css.block}>
-        <div className={css.left}><LeftBlock { ...props } /></div>
-        <div className={css.center}><CenterBlock { ...props } /></div>
-        <div className={css.right}><RightBlock/></div>
-    </div>
+    <CSSTransitionGroup
+      transitionName="content-wrapper"
+      transitionEnter={false}
+      transitionLeavet={false}
+      transitionAppear={true}
+      transitionAppearTimeout={5000}>
+      <div key="main-example" className={css.block}>
+          <div className={css.left}><LeftBlock { ...props } /></div>
+          <div className={css.center}><CenterBlock { ...props } /></div>
+          <div className={css.right}><RightBlock/></div>
+      </div>
+    </CSSTransitionGroup>
   );
 };
 
